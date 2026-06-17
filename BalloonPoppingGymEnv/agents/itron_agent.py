a73331845cb7ca9f6a041d6f700a93b8cf155693
zebra314 <kidalfonso@gmail.com>
2026-06-17 20:48:16 +0800

    Remove control limits

--- BalloonPoppingGymEnv/agents/itron_agent.py
+++ BalloonPoppingGymEnv/agents/itron_agent.py
@@ -16,11 +16,6 @@ class ITronAgent(BaseAgent):
         self.navigator = Navigator(given_parameters)
         self.controller = Controller(given_parameters)
 
-        control_cfg = given_parameters["rocket"]["control"]
-        self.max_gimbal = control_cfg["gimbal_range"]
-        self.max_roll = control_cfg["max_roll_torque"]
-        self.throttle_min = control_cfg["throttle_range"][0]
-        self.throttle_max = control_cfg["throttle_range"][1]
 
     def reset(self) -> None:
         self.estimator.reset()
@@ -38,11 +33,6 @@ class ITronAgent(BaseAgent):
         t = observation["simulation_time"]
         is_launched = t >= self.selector.get_launch_time(observation)
         launch_inclination_heading = self.selector.get_launch_heading(observation)
-
-        # Set control limits
-        tvc = np.clip(tvc, -self.max_gimbal, self.max_gimbal)
-        roll = np.clip(roll, -self.max_roll, self.max_roll)
-        throttle = np.clip(throttle, self.throttle_min, self.throttle_max)
 
         return {
             "launch": is_launched,
